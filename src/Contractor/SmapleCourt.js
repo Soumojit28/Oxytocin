f497c5f71b33450e3636940c56790dde435cd259
Soumojit <soumojitash@gmail.com>
2022-01-04 12:29:40 +0530

    added totalSupply call

--- src/Contractor/SmapleCourt.js
+++ src/Contractor/SmapleCourt.js
@@ -1,12 +1,21 @@
-import Artifact from "./api.json";
+// import Artifact from "./abi.json";
 import { ethers } from "ethers";
-const provider = new ethers.providers.Web3Provider(window.ethereum);
+// const provider = new ethers.providers.Web3Provider(window.ethereum);
+const provider = new ethers.getDefaultProvider("wss://rinkeby.infura.io/ws/v3/fe8e5ac01a944ba2a9fd24160581045a")
 const signer = provider.getSigner();
 
 const contractAddress = "0xE5e766241dcB766AaeADD4D997F7f7F2b188109b";
-
-const contract = new ethers.Contract(contractAddress, Artifact, signer);
+const abi = [{
+  "inputs": [],
+  "name": "totalSupply",
+  "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+  "stateMutability": "view",
+  "type": "function"
+}]
+const contract = new ethers.Contract(contractAddress, abi, provider);
 
 export const totalSupply = async () => {
-  return await contract.totalSupply();
+  const n= await contract.totalSupply();
+  return (n.toNumber())
+
 };
